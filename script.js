f7b1b1e077cbd2140801bd2f8f5f472ef0ad2bcc
broxthemighty <broxthemighty@gmail.com>
2025-05-12 21:11:31 -0500

    added meme pic to visibility script

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function revealMore() {
     const moreInfo = document.getElementById('moreInfo');
     const isVisible = moreInfo.style.display === 'block';
+    const memePic = document.getElementById('memePic');
   
     moreInfo.style.display = isVisible ? 'none' : 'block';
+    memePic.style.display = isVisible ?  'none' : 'block';
   }
   
   
