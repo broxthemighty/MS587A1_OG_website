dcda306ef84662cf13995d9aa7ef2085ae019a93
broxthemighty <broxthemighty@gmail.com>
2025-05-12 20:47:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function revealMore() {
+    const moreInfo = document.getElementById('moreInfo');
+    const isVisible = moreInfo.style.display === 'block';
+  
+    moreInfo.style.display = isVisible ? 'none' : 'block';
+  }
+  
+  
+  
